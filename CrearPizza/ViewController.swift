365fccea4b024d09386fc22e2e56a344d6a8d7b5
Avril  Hernández <avril@macintosh.local>
2016-04-25 22:57:11 -0500

    Initial Commit

---
+++ CrearPizza/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrearPizza
+//
+//  Created by Avril  Hernández on 25/04/16.
+//  Copyright © 2016 AHB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
